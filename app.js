f18f13c6a03c42fdcd9ab547e1adb6e2ae8bb361
Stephen Marsh <stevemarsh117@gmail.com>
2021-06-06 16:03:03 -0400

    Added fs require to app.js

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-// var message = "Hello Node!";
-// var sum = 5 + 3; 
+const fs = require("fs");
 
-// console.log(message);
-// console.log(sum);
+const generatePage = require("./src/page-template.js");
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = profileDataArr => {
-    //This
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const [name, github] = profileDataArgs;
 
-    console.log("================");
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-        console.log(profileItem)
-    });
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
